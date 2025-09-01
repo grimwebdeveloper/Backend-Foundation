e3a156ba6b5033f5f11326cb93ad6134e1bca808
SHARIF IQBAL JAMIL <msijmpshaan369@gmail.com>
2025-09-01 15:03:29 +0530

    Completed

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,53 @@
+import { useGSAP } from '@gsap/react';
 import flowerSrc from '../../public/flower.webp';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/all';
 
 const Main = () => {
+  gsap.registerPlugin(ScrollTrigger);
+  useGSAP(() => {
+    const tl = gsap.timeline();
+
+    tl.from('img', {
+      opacity: 0,
+      duration: 2,
+    });
+
+    tl.from('#heading', {
+      opacity: 0,
+    });
+
+    tl.fromTo(
+      'img',
+      { opacity: 1 },
+      {
+        opacity: 0.3,
+        scale: 1.5,
+        immediateRender: false,
+        scrollTrigger: {
+          markers: true,
+          start: 'top 150%',
+          end: 'top 0',
+          scrub: 1,
+          trigger: '#headingTwo',
+        },
+      }
+    );
+    tl.to('img', {
+      opacity: 0.3,
+      y: -250,
+      scale: 2,
+      immediateRender: false,
+      scrollTrigger: {
+        markers: true,
+        start: 'top 150%',
+        end: 'top 0',
+        scrub: 1,
+        trigger: '#headingThree',
+      },
+    });
+  });
+
   return (
     <main className='max-w-screen-xl mx-auto p-4'>
       <img
@@ -8,10 +55,16 @@ const Main = () => {
         alt='flower'
         className='fixed top-1/2 -translate-y-1/2 scale-125 sm:top-0 sm:translate-0 pointer-events-none'
       />
-      <h2 className='text-center relative font-extrabold z-50 text-[12vw] xl:text-[153.6px] md:mt-32 h-screen'>
+      <h2
+        id='heading'
+        className='text-center relative font-extrabold z-50 text-[12vw] xl:text-[153.6px] md:mt-32 h-screen text-red-100'
+      >
         Floral Fragility
       </h2>
-      <section className='min-h-screen py-8 md:py-12 xl:py-16 lg:w-1/2 lg:ml-auto md:px-16 lg:px-0'>
+      <section
+        id='headingTwo'
+        className='min-h-screen py-8 md:py-12 xl:py-16 lg:w-1/2 lg:ml-auto md:px-16 lg:px-0'
+      >
         <h2 className='text-3xl mb-8'>
           With an open mind and a strong dedication to the craft we are
           determined to fully reveal the essence of every piece we come across.
@@ -37,7 +90,10 @@ const Main = () => {
           alert: He likes Raisin Bran.
         </p>
       </section>
-      <section className='min-h-screen py-8 md:py-12 xl:py-16 flex flex-col gap-64 items-center justify-center lg:w-1/2 lg:ml-auto'>
+      <section
+        id='headingThree'
+        className='min-h-screen py-8 md:py-12 xl:py-16 flex flex-col gap-64 items-center justify-center lg:w-1/2 lg:ml-auto'
+      >
         <h2 className='text-[16vw] leading-[16vw] lg:text-[102.4px] lg:leading-[102.4px] lg:self-start'>
           Red Pottery
           <br />
