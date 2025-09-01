976a02cf5f8f09894b26f2dcd26bb6931fc9a211
SHARIF IQBAL JAMIL <msijmpshaan369@gmail.com>
2025-09-01 13:18:22 +0530

    Locomotive added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import Footer from './components/Footer';
 import Main from './components/Main';
 import Navbar from './components/Navbar';
+import LocomotiveScroll from 'locomotive-scroll';
 
 const App = () => {
+  const locomotiveScroll = new LocomotiveScroll();
   return (
     <>
       <Navbar />
